2b9c871096235c340430ff5d3c05f79802e7ae81
dan-harris1 <danielmharris3@gmail.com>
2023-01-26 10:43:08 -0800

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'a38252ea-5c56-45d8-b9cb-40ade2fca784';
+export const MODAL_CLOSE_SEC = 2.5;
